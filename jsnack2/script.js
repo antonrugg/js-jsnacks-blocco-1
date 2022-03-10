012f0f5e049ade256d1cbb520b0a8d73bc879787
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-10 16:47:15 +0100

    trying different ways - excercise 3

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,5 +1,3 @@
-console.log('js ok');
-
 // Crea un array vuoto.Chiedi per 6 volte all’utente di inserire un numero,
 // se è dispari inseriscilo nell’array.
 
--- jsnack3/script.js
+++ jsnack3/script.js
@@ -3,3 +3,22 @@ console.log('js ok');
 // Chiedi un numero di 4 cifre
 // all’utente e calcola la somma di tutte le cifre che compongono il numero.
 // (questo potrebbe richiedere un minimo di ricerca)
+
+let number = parseInt(prompt('Insert a four digits number'));
+let sum = 0;
+
+while (number) {
+    sum += number % 10;
+    number = Math.floor(number / 10);
+}
+
+console.log(sum);
+
+// let number = prompt('Insert a four digits number');
+// const numberArray = number;
+// let sum = numberArray[0];
+// for (i = 1; i < numberArray.length; i++){
+//     sum += numberArray[i];
+// }
+
+// console.log(sum);
