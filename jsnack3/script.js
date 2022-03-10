4efd8b31d3b1da1de4ebd734e6800baf8c5c69f6
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-10 20:31:12 +0100

    added while-loop check if number is 4 digits otherwise ask again input

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,24 +1,43 @@
-console.log('js ok');
-
 // Chiedi un numero di 4 cifre
 // all’utente e calcola la somma di tutte le cifre che compongono il numero.
 // (questo potrebbe richiedere un minimo di ricerca)
 
-let number = parseInt(prompt('Insert a four digits number'));
-let sum = 0;
 
-while (number) {
-    sum += number % 10;
-    number = Math.floor(number / 10);
+// let sum = 0;
+// let isValid = false;
+
+// do {
+//     let number = prompt('Insert a four digits number');
+//     if (number.length === 4)
+//         isValid = true;
+//     while (number) {
+    
+//         sum += number % 10;
+//         number = Math.floor(number / 10);
+//     }
+// }while(!isValid)
+// console.log(sum);
+
+let isValid = false;
+while (!isValid) {
+    let number = prompt('Insert a four digits number');
+    if (number.length === 4) {
+        isValid = true;
+        let numberArray = Array.from(String(number), Number);
+        let sum = 0;
+        for (i = 0; i < numberArray.length; i++) {
+            sum += numberArray[i];
+        }
+        console.log(sum);
+    }
 }
 
-console.log(sum);
-
 // let number = prompt('Insert a four digits number');
-// const numberArray = number;
-// let sum = numberArray[0];
-// for (i = 1; i < numberArray.length; i++){
-//     sum += numberArray[i];
+// let sum = 0;
+// const numbers = number;
+// for (const num of numbers) {
+//     console.log(num);
+//     sum += num;
+    
 // }
-
 // console.log(sum);
